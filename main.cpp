c50a093098aa83be5b8ebbfee65a1f7af2e643dc
nmq-hyt <conoroboyle83@gmail.com>
2021-08-24 13:29:44 +0100

    program now properly works!

--- main.cpp
+++ main.cpp
@@ -9,15 +9,15 @@
 using std::string;
 using std::cout;
 using std::endl;
-using std::bitset;
 
 int main (int argc, char* argv[]) {
 
   // convert the cmdline argument into a string
   string text = argv[1];
-  string encoded_text = base64_encode(test);
+  string encoded_text = base64_encode(text);
   cout << encoded_text << endl;
-
+  text = base64_decode(encoded_text);
+  cout << text << endl;
 
   return EXIT_SUCCESS;
 }
